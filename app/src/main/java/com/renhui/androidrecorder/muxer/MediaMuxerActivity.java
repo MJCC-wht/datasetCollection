66475ff4e79c68b81fba1fe71d288458d0f61d52
yunming-ls <294602112@qq.com>
2023-03-24 16:01:29 +0800

    把要用的图片和视频封装在apk里; 并使得不同的按钮跳转到不同的视频和图片

--- app/src/main/java/com/renhui/androidrecorder/muxer/MediaMuxerActivity.java
+++ app/src/main/java/com/renhui/androidrecorder/muxer/MediaMuxerActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.app.ActionBar;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -100,45 +101,7 @@ public class MediaMuxerActivity extends AppCompatActivity implements SurfaceHold
         Intent intent = getIntent();
         filePath = intent.getStringExtra("complete_info");
         filePathList = filePath.split("/");
-        switch (filePathList[1]) {
-            case "action1":
-                VoiceBroadcastThread.stopBroadcast();
-                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请正常行走，维持十秒左右");
-                break;
-            case "action2":
-                VoiceBroadcastThread.stopBroadcast();
-                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请从椅子上站起，再坐下。");
-                break;
-            case "action3":
-                VoiceBroadcastThread.stopBroadcast();
-                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请上台阶，维持十秒左右。");
-                break;
-            case "action4":
-                VoiceBroadcastThread.stopBroadcast();
-                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请顺时针三百六十度转身，再逆时针三百六十度转身。");
-                break;
-            case "recognition1": case "recognition2": case "recognition3":
-            case "recognition4": case "recognition5":
-            case "emotion1": case "emotion2": case "emotion3":
-            case "emotion4": case "emotion5":
-                VoiceBroadcastThread.stopBroadcast();
-                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请完整观看目标视频。");
-                break;
-            case "description1":
-                VoiceBroadcastThread.stopBroadcast();
-                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请在一分钟内，说出尽可能多的水果名称。");
-                break;
-            case "description2":
-                VoiceBroadcastThread.stopBroadcast();
-                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请在一分钟内，描述目标图片的场景。");
-                break;
-            case "description3":
-                VoiceBroadcastThread.stopBroadcast();
-                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请在三分钟内，回忆并简述刚才听到的故事。");
-                break;
-            default:
-                break;
-        }
+        switchBroadcast(filePathList[1]);
 
         // 选择是否需要小窗口  yes or no
         floatWindow = intent.getStringExtra("cameraWindow") ;
@@ -192,7 +155,7 @@ public class MediaMuxerActivity extends AppCompatActivity implements SurfaceHold
                     }
                     FileUploadThread.stopUpload();
                     if (confirmType(filePathList[1]) == RECOGNITION_TYPE || confirmType(filePathList[1]) == EMOTION_TYPE) {
-                        VideoPlayerThread.startPlay(MediaMuxerActivity.this, mVideo);
+                        VideoPlayerThread.startPlay(MediaMuxerActivity.this, mVideo, filePathList[1]);
                     }
                 }
             }
@@ -220,11 +183,7 @@ public class MediaMuxerActivity extends AppCompatActivity implements SurfaceHold
                     ((TextView) view).setText("停止录制");
                     AudioEncoderThread.startAudio(filePath);
                     FileUploadThread.stopUpload();
-                    try {
-                        updateImage(getLocalImage("file://" + Environment.getExternalStorageDirectory().getPath() + "/android_records/audioImage/1.png"));
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
+                    updateImage(getImageFromAssetsFile(switchImage(filePathList[1])));
                 }
             }
         });
@@ -437,4 +396,78 @@ public class MediaMuxerActivity extends AppCompatActivity implements SurfaceHold
             return WRONG_TYPE;
         }
     }
+
+    // 检测需要播报的内容
+    private void switchBroadcast(String text) {
+        switch (text) {
+            case "action1":
+                VoiceBroadcastThread.stopBroadcast();
+                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请正常行走，维持十秒左右");
+                break;
+            case "action2":
+                VoiceBroadcastThread.stopBroadcast();
+                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请从椅子上站起，再坐下。");
+                break;
+            case "action3":
+                VoiceBroadcastThread.stopBroadcast();
+                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请上台阶，维持十秒左右。");
+                break;
+            case "action4":
+                VoiceBroadcastThread.stopBroadcast();
+                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请顺时针三百六十度转身，再逆时针三百六十度转身。");
+                break;
+            case "recognition1": case "recognition2": case "recognition3":
+            case "recognition4": case "recognition5":
+            case "emotion1": case "emotion2": case "emotion3":
+            case "emotion4": case "emotion5":
+                VoiceBroadcastThread.stopBroadcast();
+                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请完整观看目标视频。");
+                break;
+            case "description1":
+                VoiceBroadcastThread.stopBroadcast();
+                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请在一分钟内，说出尽可能多的水果名称。");
+                break;
+            case "description2":
+                VoiceBroadcastThread.stopBroadcast();
+                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请在一分钟内，描述目标图片的场景。");
+                break;
+            case "description3":
+                VoiceBroadcastThread.stopBroadcast();
+                VoiceBroadcastThread.startBroadcast(MediaMuxerActivity.this, "请在三分钟内，回忆并简述刚才听到的故事。");
+                break;
+            default:
+                break;
+        }
+    }
+
+    // 根据点击的按钮呈现不同的图片
+    public String switchImage(String imagetext){
+        switch(imagetext) {
+            case "description1":
+                imagetext = "fruits.jpg";
+                break;
+            case "description2":
+                imagetext = "family.jpg";
+                break;
+            default:
+                break;
+        }
+        return imagetext;
+    }
+
+    // 获取assets中的图片
+    private Bitmap getImageFromAssetsFile(String fileName) {
+        Bitmap image = null;
+        AssetManager am = getResources().getAssets();
+        try {
+            InputStream is = am.open(fileName);
+            image = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (IOException e) {
+            Log.e("error", "getImageFromAssetsFile: IOException!");
+        }
+        return image;
+    }
+
+
 }
--- app/src/main/java/com/renhui/androidrecorder/muxer/VideoPlayerThread.java
+++ app/src/main/java/com/renhui/androidrecorder/muxer/VideoPlayerThread.java
@@ -2,6 +2,8 @@ package com.renhui.androidrecorder.muxer;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.graphics.Camera;
 import android.media.MediaPlayer;
 import android.os.Environment;
@@ -18,7 +20,9 @@ import android.widget.VideoView;
 import com.renhui.androidrecorder.R;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 public class VideoPlayerThread extends Thread{
@@ -27,18 +31,20 @@ public class VideoPlayerThread extends Thread{
 
     private VideoView mVideo;
     private Context mContext;
+    private String videotext;
 
-    public VideoPlayerThread(Context mContext, VideoView mVideo) {
+    public VideoPlayerThread(Context mContext, VideoView mVideo, String videotext) {
         // 初始化相关对象和参数
         this.mContext = mContext;
         this.mVideo = mVideo;
+        this.videotext = videotext;
     }
 
-    public static void startPlay(Context mContext, VideoView mVideo) {
+    public static void startPlay(Context mContext, VideoView mVideo, String videotext) {
         if (videoPlayerThread == null) {
             synchronized (VideoPlayerThread.class) {
                 if (videoPlayerThread == null) {
-                    videoPlayerThread = new VideoPlayerThread(mContext, mVideo);
+                    videoPlayerThread = new VideoPlayerThread(mContext, mVideo, videotext);
                     videoPlayerThread.start();
                 }
             }
@@ -56,6 +62,71 @@ public class VideoPlayerThread extends Thread{
         cameraparams.removeRule(RelativeLayout.ALIGN_PARENT_TOP);
         cameraparams.removeRule(RelativeLayout.ALIGN_BOTTOM);
         cameraparams.removeRule(RelativeLayout.ALIGN_LEFT);
+    }
+
+    // 选择需要播放的视频标签
+    public String switchVideo(String videotext){
+        switch(videotext) {
+            case "recognition1": case "emotion1":
+                videotext = "01maiguai.mp4";
+                break;
+            case "recognition2": case "emotion2":
+                videotext = "02maiche.mp4";
+                break;
+            case "recognition3": case "emotion3":
+                videotext = "03mancai.mp4";
+                break;
+            case "recognition4": case "emotion4":
+                videotext = "04zhangerda.mp4";
+                break;
+            case "recognition5": case "emotion5":
+                videotext = "05twins.mp4";
+                break;
+            default:
+                break;
+        }
+        return videotext;
+    }
+
+    //将assets内文件存储到本地下载目录
+    public Boolean Deposit(String path,String fileName){
+        InputStream inputStream;
+        try {
+
+            //判断文件是否存在
+            File file1 = new File(path + "/" + fileName);
+
+            if (!file1.exists()) {
+
+                inputStream = mContext.getAssets().open(fileName);
+                File file = new File(path);
+
+                //当目录不存在时创建目录
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+
+                FileOutputStream fileOutputStream = new FileOutputStream(path + "/" + fileName);// 保存到本地的文件夹下的文件
+                byte[] buffer = new byte[1024];
+                int count = 0;
+                while ((count = inputStream.read(buffer)) > 0) {
+                    fileOutputStream.write(buffer, 0, count);
+                }
+                fileOutputStream.flush();
+                fileOutputStream.close();
+                inputStream.close();
+
+            } else {
+                Log.e("fileexist","目标视频文件已存在");
+            }
+
+            return true;
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
 
@@ -77,8 +148,11 @@ public class VideoPlayerThread extends Thread{
             switch(msg.what){
                 case 1:
                     // 根据上面的提示，当Message为1，表示数据处理完了，可以通知主线程了
+                    // 下载目标视频
+                    videotext = switchVideo(videotext);
+                    Deposit(Environment.getExternalStorageDirectory() + "/android_records/targetvideo", videotext);
                     // 加载本地视频
-                    String videoPath = Environment.getExternalStorageDirectory() + "/android_records/video/smile1.mp4";  //2023_03_17_17_31.mp4
+                    String videoPath = Environment.getExternalStorageDirectory() + "/android_records/targetvideo/" + videotext;
                     File file = new File(videoPath);
                     if (file.exists()) {
                         // 设置视频地址
