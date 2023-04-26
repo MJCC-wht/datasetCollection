eaf653b0d1148ac362bb46dfee3f2c67c57e0bc6
yunming-ls <294602112@qq.com>
2023-04-26 19:57:25 +0800

    添加了量表测评的activity

---
+++ app/src/main/java/com/renhui/androidrecorder/survey/HealthSurveyActivity.java
@@ -0,0 +1,15 @@
+package com.renhui.androidrecorder.survey;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.renhui.androidrecorder.R;
+
+public class HealthSurveyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_healthsurvey);
+
+    }
+}
